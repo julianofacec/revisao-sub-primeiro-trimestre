dd38930ac06089101ec3e2e9b37536b6fcd85b7d
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-07-06 19:21:03 -0300

    Adicionado arquivo .gitignore

---
+++ Revisao.Sub.Primeiro.Trimestre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Revisao.Sub.Primeiro.Trimestre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
